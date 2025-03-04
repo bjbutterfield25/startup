eb7ecaa2e9aaa25be43f6e18eea631db88d6d81a
Bradley <194412712+bjbutterfield25@users.noreply.github.com>
2025-03-04 11:06:40 -0700

    Set up apiRouter

--- service/index.js
+++ service/index.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+let users = [];
+let comments = [];
+
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
