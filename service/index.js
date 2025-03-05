44b5070499015368d75e6ad407e3cb2cd7a3e223
Bradley <194412712+bjbutterfield25@users.noreply.github.com>
2025-03-05 10:37:08 -0700

    Finished adding the required modules

--- service/index.js
+++ service/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const uuid = require('uuid');
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+
+const authCookieName = 'token';
+
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 let users = [];
 let comments = [];
 
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
 
 var apiRouter = express.Router();
