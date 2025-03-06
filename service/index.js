de276d5e5f559549dd1aff9239c3a42c1b7608f5
Bradley <194412712+bjbutterfield25@users.noreply.github.com>
2025-03-06 11:11:49 -0700

    Added functions to create new users on the backend

--- service/index.js
+++ service/index.js
@@ -19,6 +19,33 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('username', req.body.username)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.username, req.body.password);
+        res.send({ username: user.username });
+    }
+});
+
+async function createUser(username, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = { 
+        username: username, 
+        password: passwordHash,
+        token: uuid.v4()
+    };
+    users.push(user);
+    return user;
+}
+
+async function findUser(key, value) {
+    if(!value) {
+        return null;
+    }
+    return users.find(user => user[key] === value);
+}
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -10,10 +10,26 @@ export function Unauthenticated(props) {
     }
   
     async function createUser() {
-      localStorage.setItem('userName', userName);
-      props.onLogin(userName);
+      loginOrCreate(`/api/auth/create`);
     }
   
+    async function loginOrCreate(endpoint) {
+      const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ email: userName, password: password }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      if (response?.status === 200) {
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
+      } else {
+        const body = await response.json();
+        alert(`⚠ Error: ${body.msg}`);
+      }
+    }
+
     return (
       <>
         <p className="intro">Pictures Around the World allows you to view various photos and leave comments if you desire. Your comments will be public to all other users and you must login below in order to leave a comment.</p>
