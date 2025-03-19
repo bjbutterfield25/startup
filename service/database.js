592acae045bee41aebff8f6d792fa5a7365cb62d
Bradley <194412712+bjbutterfield25@users.noreply.github.com>
2025-03-18 20:54:34 -0600

    Added test connection function

--- service/database.js
+++ service/database.js
@@ -6,3 +6,13 @@ const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
 const commentCollection = db.collection('comment');
+
+(async function testConnection() {
+    try {
+      await db.command({ ping: 1 });
+      console.log(`Connect to database`);
+    } catch (ex) {
+      console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+      process.exit(1);
+    }
+  })();
